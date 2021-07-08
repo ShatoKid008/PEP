1be2adc7fd61041f6555d59e32f9d263ba266d32
ShatoKid008 <83462531+ShatoKid008@users.noreply.github.com>
2021-07-08 19:51:29 +0100

    Add files via upload
    Module 3 of PEP complete

--- Programming Essentials in Python.py
+++ Programming Essentials in Python.py
@@ -209,3 +209,49 @@
 
 
 
+# the basics of lists lab
+# beatles = []# step 1  create an empty list named beatles
+# print("Step 1:", beatles)
+#
+# beatles.append("John Lennon") # step 2 use the append() method to add the following members of the band to the list: John Lennon, Paul McCartney, and George Harrison
+# beatles.append("Paul McCartney")
+# beatles.append("George Harrison")
+# print("Step 2:", beatles)
+#
+# for i in range(0,2): # step 3 use the for loop and the append() method to prompt the user to add the following members of the band to the list: Stu Sutcliffe, and Pete Best
+#     beatles.append(input("Enter a beatle: "))
+#     continue
+# print("Step 3:", beatles)
+#
+# del beatles[-1]# step 4 use the del instruction to remove Stu Sutcliffe and Pete Best from the list
+# del beatles[-1]
+# print("Step 4:", beatles)
+#
+# beatles.insert(0, "Ringo Starr")# step 5 use the insert() method to add Ringo Starr to the beginning of the list
+# print("Step 5:", beatles)
+#
+#
+# # testing list length
+# print("The Fab", len(beatles))
+
+
+
+# # program to remove repeated numbers from a user inputed list
+# my_list = []
+# new_list = []
+# list_size = int(input("How many numbers are in your list?: "))
+#
+# for i in range (list_size):
+#     entry = int(input("Enter numbers: "))
+#     my_list.append(entry)
+#
+# for number in my_list:
+#     if number not in new_list:
+#         new_list.append(number)
+#
+# my_list = new_list[:]
+#
+# print("The list with unique elements only:")
+# print(my_list)
+
+
